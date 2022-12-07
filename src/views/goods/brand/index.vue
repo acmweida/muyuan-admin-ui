dff7274ad9a26d7229b0fb3fa54444d492e29d07
weida <weida@eptok.com>
2022-12-07 17:36:15 +0800

    feat #69 品牌查询

--- src/views/goods/brand/index.vue
+++ src/views/goods/brand/index.vue
@@ -118,7 +118,7 @@
           <dict-tag :options="dict.type.product_brand_audit_status" :value="scope.row.auditStatus"/>
         </template>
       </el-table-column>
-      <el-table-column label="备注" align="center" prop="remark"/>
+<!--      <el-table-column label="备注" align="center" prop="remark"/>-->
       <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
         <template slot-scope="scope">
           <el-button
